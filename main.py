1e005117594abc9452b347a674687745731ec775
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-25 15:18:47 +0530

    Day25

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 import sklearn
+
+# //Apple: 0 and Orange: 1
+features = [[140, 1], [130, 1], [150, 0], [170, 0]]
+labels = [0,0,1,1]
+
+# // Using Decision Modal tree
+clf = tree.DecisionTreeClassifier()
+# // categoring the content i.e feature and labels
+clf = clf.fit(features,labels)
